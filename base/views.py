b10e9492e200dcb191eb6fb70e5d3050926db9cf
JamalEK12 <jamals29834@gmail.com>
2026-03-04 20:44:28 +0000

    Update

--- base/views.py
+++ base/views.py
@@ -2,8 +2,26 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout 
+from django.shortcuts import redirect
+
 from .models import Task
+
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+       template_name = 'base/login.html'
+       fields = '__all__'
+       redirect_authenticated_user = True
+
+       def get_success_url(self):
+              return reverse_lazy('tasks')
+       
+        
 
 class Tasklist(ListView):
       model = Task
