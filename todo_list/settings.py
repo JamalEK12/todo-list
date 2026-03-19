04fbe0ebd46625b622e2e16827b05eb92f19b469
JamalEK12 <jamals29834@gmail.com>
2026-03-19 21:06:51 +0000

    Heroku update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -32,8 +32,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = [
+    'jamal-todo1-dd2d34670e9f.herokuapp.com',
+    '127.0.0.1',
+    'localhost',
+    '.herokuapp.com'
+]
 
 
 INSTALLED_APPS = [
