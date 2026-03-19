19582115c48fcb97d88cacbecbcac23cacb7e0c5
JamalEK12 <jamals29834@gmail.com>
2026-03-19 22:52:45 +0000

    Updated settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 if os.environ.get("DJANGO_DEVELOPMENT"):
     load_dotenv()
